29951badc0f74b03f20b1194d288e0ad8582e326
Kirill Kovalev <kirill.kovalev1502@mail.ru>
2022-06-14 17:12:00 +0300

    feat: add header section

--- photo-stock/src/components/Header.jsx
+++ photo-stock/src/components/Header.jsx
@@ -5,12 +5,14 @@ function Header() {
     <div className="header">
       <div className="container">
         <div className="header-content">
-          <h1>The best free stock photos, royalty free images &amp; videos shared by creators.</h1>
+          <h1 className="header-content-title">
+            The best free stock photos &amp; videos shared by talented creators.
+          </h1>
           <div className="header-search-container">
             <input
               type="search"
               class="header-content-search"
-              placeholder="Search for free photos"
+              placeholder="Search for free photos and videos"
             />
             <button className="header-search-btn">
               <img
@@ -19,7 +21,75 @@ function Header() {
                 alt="search-button"></img>
             </button>
           </div>
+          <div className="header-content-trending">
+            <span className="header-content-trending-title">Suggested:</span>
+            <ul className="header-content-trending-items">
+              <li>
+                <a
+                  className="header-content-trending-item"
+                  href="https://www.pexels.com/search/business/">
+                  business
+                </a>
+                <span>,&nbsp;</span>
+              </li>
+              <li>
+                <a
+                  className="header-content-trending-item"
+                  href="https://www.pexels.com/search/space/">
+                  space
+                </a>
+                <span>,&nbsp;</span>
+              </li>
+              <li>
+                <a
+                  className="header-content-trending-item"
+                  href="https://www.pexels.com/search/food/">
+                  food
+                </a>
+                <span>,&nbsp;</span>
+              </li>
+              <li>
+                <a
+                  className="header-content-trending-item"
+                  href="https://www.pexels.com/search/summer/">
+                  summer
+                </a>
+                <span>,&nbsp;</span>
+              </li>
+              <li>
+                <a
+                  className="header-content-trending-item"
+                  href="https://www.pexels.com/search/technology/">
+                  technology
+                </a>
+                <span>,&nbsp;</span>
+              </li>
+              <li>
+                <a
+                  className="header-content-trending-item"
+                  href="https://www.pexels.com/search/forest/">
+                  forest
+                </a>
+                <span>,&nbsp;</span>
+              </li>
+              <li>
+                <a
+                  className="header-content-trending-item"
+                  href="https://www.pexels.com/search/beach/">
+                  beach
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
+        <a
+          class="header-photo-publisher"
+          href="https://www.pexels.com/photo/landscape-sky-hands-woman-11325889/"
+          target="_blank"
+          rel="noreferrer">
+          <span className="header-photo-publisher-title">Photo by&nbsp;</span>
+          <span className="header-photo-publisher-name">Sebastiaan Stam</span>
+        </a>
         <img
           className="header-background"
           src="https://images.pexels.com/photos/11325889/pexels-photo-11325889.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=500&w=1400&dpr=1"
