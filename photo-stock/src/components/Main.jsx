104dacfb65d70ad4a21a0d9e6512906187c14769
Kirill Kovalev <kirill.kovalev1502@mail.ru>
2022-06-15 15:55:06 +0300

    feat: add image info container in main section

--- photo-stock/src/components/Main.jsx
+++ photo-stock/src/components/Main.jsx
@@ -7,6 +7,9 @@ import image4 from '../assets/img/photo4.jpg';
 import image5 from '../assets/img/photo5.jpg';
 import image6 from '../assets/img/photo6.jpg';
 
+import btnHeart from '../assets/img/heart.png';
+import btnDownload from '../assets/img/download.png';
+
 function Main() {
   return (
     <main className="main">
@@ -16,16 +19,42 @@ function Main() {
         </div>
         <div className="images-container">
           <div className="images-column">
-            <img src={image1} alt="images-column-item"></img>
-            <img src={image4} alt="images-column-item"></img>
+            <div className="images-column-item">
+              <div className="image-column-item">
+                <img src={image1} alt="photos-item"></img>
+              </div>
+              <div className="item-info">
+                <div className="item-info-author">
+                  <img
+                    src="https://images.pexels.com/users/avatars/224803103/abraham-perez-549.jpeg?auto=compress&fit=crop&h=130&w=130&dpr=1"
+                    alt="item-photos-autor"></img>
+                  <p>Abraham Pérez</p>
+                </div>
+                <div className="item-info-btns">
+                  <img src={btnHeart} alt="btn-heart"></img>
+                  <img src={btnDownload} alt="btn-download"></img>
+                </div>
+              </div>
+            </div>
+            <div className="images-column-item">
+              <img src={image4} alt="photos-item"></img>
+            </div>
           </div>
           <div className="images-column">
-            <img src={image2} alt="images-column-item"></img>
-            <img src={image5} alt="images-column-item"></img>
+            <div className="images-column-item">
+              <img src={image2} alt="photos-item"></img>
+            </div>
+            <div className="images-column-item">
+              <img src={image5} alt="photos-item"></img>
+            </div>
           </div>
           <div className="images-column">
-            <img src={image3} alt="images-column-item"></img>
-            <img src={image6} alt="images-column-item"></img>
+            <div className="images-column-item">
+              <img src={image3} alt="photos-item"></img>
+            </div>
+            <div className="images-column-item">
+              <img src={image6} alt="photos-item"></img>
+            </div>
           </div>
         </div>
       </div>
---
+++ photo-stock/src/components/SearchForm.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function SearchForm() {
+  return (
+    <div className="header-search-container">
+      <input
+        type="search"
+        class="header-content-search"
+        placeholder="Search for free photos and videos"
+      />
+      <button className="header-search-btn">
+        <img
+          className="header-search-btn-image"
+          src="https://cdn-icons.flaticon.com/png/512/4024/premium/4024513.png?token=exp=1655210527~hmac=6df853562d1deebd4c30d6467ec306c6"
+          alt="search-button"></img>
+      </button>
+    </div>
+  );
+}
+
+export default SearchForm;
