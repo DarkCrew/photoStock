609a98a4c8a4861a6b80900d9c4bc0b7194053cb
Kirill Kovalev <kirill.kovalev1502@mail.ru>
2022-06-15 13:30:47 +0300

    feat: add navbar animation and navbar search

--- photo-stock/src/components/Navbar.jsx
+++ photo-stock/src/components/Navbar.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/img/pexels-logo.svg';
 
 function Navbar() {
+  const [navbar, setNavbar] = useState(false);
+
+  const changeNavbarBackground = () => {
+    window.scrollY > 80 ? setNavbar(true) : setNavbar(false);
+  };
+
+  window.addEventListener('scroll', changeNavbarBackground);
+
   return (
-    <div className="navbar">
+    <div className={navbar ? 'navbar active' : 'navbar'}>
       <div className="container">
         <nav className="navbar-container">
           <div className="logo">
             <img className="logo-image" src={logo} alt="logo"></img>
             <span>Pexels</span>
+          </div>
+          <div className={navbar ? 'navbar-search-container active' : 'navbar-search-container'}>
+            <input
+              type="search"
+              class="navbar-content-search"
+              placeholder="Search for free photos and videos"
+            />
+            <button className="navbar-search-btn">
+              <img
+                className="navbar-search-btn-image"
+                src="https://cdn-icons.flaticon.com/png/512/4024/premium/4024513.png?token=exp=1655210527~hmac=6df853562d1deebd4c30d6467ec306c6"
+                alt="search-button"></img>
+            </button>
           </div>
           <ul className="navbar-items">
             <li>
